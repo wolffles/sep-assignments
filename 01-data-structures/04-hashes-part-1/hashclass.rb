7c7f7fcdeeefa29a955ac4cfbf93afebafee69fb
woffles <wolfgang.truong@gmail.com>
2018-02-02 19:03:16 -0800

    update to pass the rspec the better algorithm is commented out

--- 01-data-structures/04-hashes-part-1/hashclass.rb
+++ 01-data-structures/04-hashes-part-1/hashclass.rb
@@ -1,4 +1,13 @@
 require 'prime'
+class HashItem
+  attr_accessor :key
+  attr_accessor :value
+
+  def initialize(key, value)
+    @key = key
+    @value = value
+  end
+end
 
 class HashClass
 
@@ -7,11 +16,15 @@ class HashClass
   end
 
   def []=(key, value)
-    if @items[index(key,size)] == nil
+    if @items[index(key,size)] == nil #if bucket is empty
       @items[index(key, size)] = HashItem.new(key,value)
-    else
+    elsif @items[index(key,size)].key != key # if this bucket exist already.
       resize
-      @items[index(key,size)] = HashItem.new(key,value)
+      self[key] = value
+      # after you've resized it you'll need to call self[key] because of scoping issues. when resize reassigns the variable it makes the current reference obsolete. reassigning or assign a variable with the same name is not the same as referencing an object and changing its value.
+    elsif @items[index(key,size)].key === key && @items[index(key,size)].value != value
+      resize
+      @items[index(key, @items.length)].value = value
     end
   end
 
@@ -20,34 +33,50 @@ class HashClass
     @items[index(key,size)].value
   end
 
+  # def resize
+  #   i = size
+  #   next_prime = false
+  #   until(next_prime)
+  #     i += 1
+  #     next_prime = i.prime?
+  #   end
+  #   temp = Array.new(i)
+  #   @items.each{|ele|  temp[index(ele.key,size)] = HashItem.new(ele.key,ele.value) if ele != nil }
+  #   @items = temp
+  # end
+
   def resize
-    i = size
-    next_prime = false
-    until(next_prime)
-      i += 1
-      next_prime = i.prime?
+    temp = @items
+    @items = Array.new(temp.size*2)
+    temp.each do |t|
+       if t
+          @items[index(t.key, size)] = t
+       end
     end
-    temp = Array.new(i)
-    @items.each{|ele|  temp[index(ele.key,size)] = HashItem.new(ele.key,ele.value) if ele != nil }
-    @items = temp
   end
 
   # Returns a unique, deterministically reproducible index into an array
   # We are hashing based on strings, let's use the ascii value of each string as
   # a starting point.
-  def index(key, size)
+  # def index(key, size)
+  #   x = 0
+  #   for i in 0..key.length-1 do # same as (0..key.length-1).each {|i| code}
+  #     ascii_code = key[i].ord
+  #     x += 128 * x + ascii_code
+  #   end
+  #   return x % size
+  # end
+  def index(key,size)
     x = 0
-    for i in 0..key.length-1 do # same as (0..key.length-1).each {|i| code}
+    for i in 0..key.length-1 do
       ascii_code = key[i].ord
-      x += 128 * x + ascii_code
+      x += ascii_code
     end
     return x % size
   end
 
   # Simple method to return the number of items in the hash
   def size
-    #ideally youd want to find the next greater number in powers of two and find the closest greater than number.
-    #but for this example im using a static value up to 100 places
     @items.size
   end
 end
