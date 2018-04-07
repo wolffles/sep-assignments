35acb4e325e5e515a49ea803e3431a810171745b
woffles <wolfgang.truong@gmail.com>
2018-04-06 19:40:08 -0700

    revising submission

--- 02-algo-time/06-improving-complexity/improving_complexity_version_three.rb
+++ 02-algo-time/06-improving-complexity/improving_complexity_version_three.rb
@@ -1,29 +1,30 @@
 
 
-def space_written_ruby(array)
-  min = array[0]
-  max = array[0]
-  array.each{|ele|
-    min = ele if ele < min
-    max = ele if ele > max
-  }
+def space_written_ruby(*arrays)
+  flattened = []
+  min,max = arrays[0][0], arrays[0][0]
+  arrays.each do |array|
+    array.each do |value|
+      min = value if value < min
+      max = value if value > max
+      flattened << value
+    end
+  end
   count_hash = (min..max).each_with_object({}){ |i, result| result[i] = 0 }
-  #counts each element
-  array.each{|ele| count_hash[ele] += 1}
+  flattened.each{|ele| count_hash[ele] += 1}
   memo = 0
   #sums elements up to sort
   count_hash.keys.each{|ele|
     count_hash[ele] += memo
     memo = count_hash[ele]
   }
-  sorted = Array.new(array.size)
+  sorted = Array.new(flattened.size)
   #sort by adding to empty array using values in count_hash
-  array.each{|x|
+  flattened.each{|x|
     sorted[count_hash[x]] = x
     count_hash[x] -= 1
   }
-  # p sorted.compact
   sorted.compact
 end
 
-# complexity_written_ruby([3,6,1,2,9,8,5,4,7])
+# space_written_ruby([3,6,1],[2,9,8],[5,4,7]) == [1,2,3,4,5,6,7,8,9]
