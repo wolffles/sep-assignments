b79f3e77e103fac859fe30d48ab08fbba6ad9a9d
woffles <wolfgang.truong@gmail.com>
2018-04-17 15:45:41 -0700

    tsp brute force and greed grouping algorithms

--- 02-algo-time/07-complex-problem/map-data.rb
+++ 02-algo-time/07-complex-problem/map-data.rb
@@ -1,56 +1,45 @@
-range = (1..100).to_a
-x_plane = (1..100).to_a
-y_plane = (1..100).to_a
-sample_cities = ('a'..'z').to_a
-arbitrary_map = {}
-small_map = {}
-large_map = {}
+require 'byebug'
 
-arbitrary_map['a'] = Hash[
-  'b',10,
-  'c',15,
-  'd',20,
-]
-arbitrary_map['b'] = Hash[
-  'a',10,
-  'c',35,
-  'd',25,
-]
-arbitrary_map['c'] = Hash[
-  'a',15,
-  'b',35,
-  'd',30,
-]
-arbitrary_map['d'] = Hash[
-  'a',20,
-  'b',25,
-  'c',30,
-]
+#hash_format['a'] = {:coords = [x,y], :closest = [], "a" = nil, "b" = nil ... "z" = nil}
 
-#hash_format['a'] = {cord = [x,y], "a" = nil, "b" = nil ... "z" = nil}
+#binary_insert for updating_closest
+ def binary_insert(array, val)
+   return 0 if array.empty?
+   low, high = 0, array.length - 1
+   while low <= high
+     mid = ((low + high) / 2.0).ceil
+     if val < array[mid][1]
+       high = mid - 1.0
+     elsif val > array[mid][1]
+       low = mid + 1.0
+     else
+       return mid
+     end
+   end
+   return ((low + high) / 2.0).ceil
+ end
 
+#updates closest attribute for a city
 def update_closest(array, city, new_dist)
-  return array << [city,new_dist] if array.size < 3
-  if new_dist < array[-1][1]
-    array.pop
-    i = 0
-    while i < array.size
-      if new_dist < array[i][1]
-        array.insert(i,[city,new_dist])
-        break
-      end
-      i += 1
-    end
+  if !array.include?([city, new_dist])
+    insert_point = binary_insert(array, new_dist)
+    array.insert(insert_point,[city, new_dist])
   end
 end
 
+#determins distance
 def distance(map,a,b)
-  return map[a][b] if map[a][b] != nil
   a_coords = map[a][:coords]
   b_coords = map[b][:coords]
   a_closest = map[a][:closest]
   b_closest = map[b][:closest]
   ax,ay,bx,by = a_coords[0],a_coords[1], b_coords[0], b_coords[1]
+
+  if map[a][b] != nil
+    dist = map[a][b]
+    update_closest(a_closest, b, dist)
+    return dist
+  end
   if ax == bx || ay == by
     dist = [(ax - bx).abs, (ay - by).abs].max
     map[a][b],map[b][a] = dist, dist
@@ -65,19 +54,17 @@ def distance(map,a,b)
     return dist
   end
 end
-
-def create_map(city_hash, n)
+#create the map
+def create_map(map, n)
   range = (1..100).to_a
   sample_cities = ('a'..'z').to_a
   cities = sample_cities.slice(0,n)
-  cities.each{|ele| city_hash[ele] = Hash[
+  cities.each{|ele| map[ele] = Hash[
     :coords, [range.sample,range.sample],
     :closest, []
     ]
   }
+  #checking distance between all values to create a :closest attribute and distance values
+  combination = cities.combination(2).to_a
+  combination.each{|link| distance(map,link[0],link[1])}
 end
-  # create hash of x cities, each with its own [x,y] cord.
-  # each city has its own distance map
-  # (possibly find distance before tps call)
-  # each city has a closest cities value.
-  # distance for corresponding cities get added at the same time to save time.
